1525a59ebaa839dd4f50ec2045fd9d4fd6fc5055
KUD <kasa7qi@gmail.com>
2023-07-14 19:15:02 +0900

    call captcha api

--- src/RegisterWindow.tsx
+++ src/RegisterWindow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Button, Alert, Chip,
     Dialog, DialogContent, DialogActions, DialogTitle, TextField,
@@ -8,6 +8,7 @@ import { Settings } from './types'
 import { ThemeMode } from './theme/index';
 import { useThemeSwicher } from './theme/ThemeSwitcher';
 import { Trans, useTranslation } from 'react-i18next'
+import { handleSSE } from './client'
 
 const { useEffect } = React
 interface Props {
@@ -20,17 +21,58 @@ export default function RegisterWindow(props: Props) {
 
     const [, { setMode }] = useThemeSwicher();
 
-    const [openRegisterWindow, setOpenRegisterWindow] = React.useState(false)
+    const [msg, setMsg] = React.useState('')
 
-    const onRegister = () => {
-        props.close()
+    const onRegister = async () => {
+        try {
+        const response = await fetch(`https://bot100.app:7001/api/v1/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            mode: 'no-cors',
+            body: JSON.stringify({
+                captcha: "111",
+                captcha_id: "111",
+                email: "111",
+                location: "111",
+                mobile: "111",
+                nickname: "111",
+                password: "111",
+            }),
+        });
+        await handleSSE(response, (message) => {
+            if (message === '[DONE]') {
+                return;
+            }
+            const data = JSON.parse(message)
+            if (data.error) {
+                setMsg(data.error)
+            }
+        })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    const onCaptcha = async () => {
+        const response = await fetch(`https://bot100.app:7001/api/v1/captcha`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            mode: 'no-cors',
+        });
+        console.log(response)
     }
 
     // @ts-ignore
     // @ts-ignore
     return (
         <Dialog open={props.open} fullWidth >
-            <DialogTitle>{t('settings')}</DialogTitle>
+            <DialogTitle>{t('register')}</DialogTitle>
             <DialogContent>
                 <TextField
                     autoFocus
@@ -49,8 +91,11 @@ export default function RegisterWindow(props: Props) {
                     variant="outlined"
                 />
             </DialogContent>
+            <p>{msg}</p>
             <DialogActions>
                 <Button onClick={props.close}>{t('cancel')}</Button>
+                <Button onClick={onRegister}>{t('register')}</Button>
+                <Button onClick={onCaptcha}>{t('captcha')}</Button>
             </DialogActions>
         </Dialog>
     );
